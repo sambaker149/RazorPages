81652b0e59d96a14905f84838101bf3f1ac8cebb
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2022-10-07 00:19:23 +0100

    Created a new input name page for the form + added minor style to Index

---
+++ RazorPages/Pages/InputName.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages.Pages
+{
+    public class InputNameModel : PageModel
+    {
+        [BindProperty]
+        public string FullName { get; set; }
+        [BindProperty]
+        public string Email { get; set; }
+        private readonly ILogger<InputNameModel> _logger;
+
+        public InputNameModel(ILogger<InputNameModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            if(String.IsNullOrWhiteSpace(FullName))
+            {
+                ViewData["Message"] = "Please Enter your Full Name";
+            }
+            else
+            {
+                ViewData["Message"] = "Thank You " + FullName + ", Your Registration has been Successful";
+            }
+        }
+    }
+}
